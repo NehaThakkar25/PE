75e718d97b64ad72a4238b3d956efb6e85ee2fc6
Neha Thakkar <74426577+NehaThakkar25@users.noreply.github.com>
2022-01-06 12:06:31 +0530

    Changed address

--- website_code/client/src/components/mainpages/footer/Footer.js
+++ website_code/client/src/components/mainpages/footer/Footer.js
@@ -13,8 +13,7 @@ function Footer(){
                 <div className="contact-us">
                     <p><i className="fas fa-phone"></i>+91 886-601-9607</p>
                     <p><i className="fa fa-instagram"></i><a href="https://www.instagram.com/parth_enterprise14/" target="_blank">@parth_enterprise14</a></p>
-                    <p><i className="fa fa-map-marker-alt"></i>Parth Enterprise,<br/>
-                    Color Box,<br />
+                    <p><i className="fa fa-map-marker-alt"></i>Parth Enterprise,<br />
                     1493/10/S Dariyapur Gate,<br />
                     Opposite Lalpuri Mara Temple, <br />
                     Dariyapur, Ahmedabad - 380004,<br />
